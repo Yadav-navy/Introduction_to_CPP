2e16ae929a8f24340e8f5a1b02f111a2bdef5308
Yadav-navy <yadav.navy03@gmail.com>
2024-06-10 23:44:15 +0530

    10_Juen

---
+++ 12.Char_Arrays_and_Strings/2.Strings.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch[10];
+    cin >> ch;
+    cout << ch << endl;
+    string s ;
+    s = "Navneet Yadav";
+    cout << s << endl;
+    cout << s.size() << endl;
+}
