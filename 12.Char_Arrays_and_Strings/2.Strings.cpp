e754c57501fa49fff33b5915add89a8ccff648ae
Yadav-navy <yadav.navy03@gmail.com>
2024-06-12 10:03:24 +0530

    12_June

--- 12.Char_Arrays_and_Strings/2.Strings.cpp
+++ 12.Char_Arrays_and_Strings/2.Strings.cpp
@@ -3,10 +3,28 @@ using namespace std;
 
 int main(){
     char ch[10];
-    cin >> ch;
+    // cin >> ch;
+    cin.getline(ch,100);
     cout << ch << endl;
     string s ;
     s = "Navneet Yadav";
     cout << s << endl;
     cout << s.size() << endl;
-}
+    cout << INT_MIN << endl;
+
+    int arr[5] = {0};
+    for (int i =0 ; i< 5; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    if (s.substr(1,3)=="av" ){
+        cout << "equal" << endl;
+        
+    }
+    cout << s.substr(2,3) << endl;
+    s.erase(2,3);
+    cout << s << endl;
+
+    cout << s.find("Yad") << endl;
+    }
---
+++ 12.Char_Arrays_and_Strings/3.Char_array_comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr_1[3]={1,2,3};
+    int arr_2[3]={1,2,3};
+
+        
+    if (arr_1 == arr_2){
+        cout << " equal" << endl;
+    }
+    
+    
+}
